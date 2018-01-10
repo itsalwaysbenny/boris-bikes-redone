a6cf16ea79d270ac768caa546048f68fd0d63c15
Ben Taylor <taylor.ben89@gmail.com>
2018-01-10 16:15:38 +0000

    First commit for a redone Boris Bikes
    Redoing Boris bikes to make a step forward

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,15 @@
+require 'docking_station'
+require 'bike'
+
+describe DockingStation do
+
+  it "responds to release_bike" do
+    expect(subject).to respond_to :release_bike
+  end
+
+  it "relseases a working bike" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
+end
