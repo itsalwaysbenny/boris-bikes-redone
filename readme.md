d14799da5211b4d2d518b995ac1aa7d8a9dd5932
Ben Taylor <taylor.ben89@gmail.com>
2018-01-11 16:07:33 +0000

    completed step 12 and added to readme

---
+++ readme.md
@@ -0,0 +1,21 @@
+## Boris Bikes
+
+A project to emulate interaction between bikes and docking stations with the ability to:
+
+- Dock a bike
+- Release a bikes
+- Check availability of bikes
+- Check if bikes are working
+
+The concepts used develop this project were:
+- User Stories
+- Domain Models
+- TDD
+- OOP
+- Single Responsibility Principle
+
+### Technologies used
+
+- Ruby
+- Rspec
+- Github
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,9 +7,18 @@ describe DockingStation do
     expect(subject).to respond_to :release_bike
   end
 
-  it "releseases a working bike" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe "#release_bike" do
+    it "releseases a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+  end
+
+  describe "#release_bike" do
+    it "raises an error when there are no bikes available" do
+      expect { subject.release_bike}.to raise_error "No bikes available"
+    end
   end
 
   it "responds to dock" do
@@ -30,5 +39,6 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
+
 
 end
