061722b1fd70b394ec1b9ad3e0b05b9c6ef65488
Ben Taylor <taylor.ben89@gmail.com>
2018-01-20 16:05:00 +0000

    Completed step 16

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -31,7 +31,7 @@ describe DockingStation do
 
   describe "#dock" do
     it "rasies an error when docking station is full" do
-      20.times { subject.dock Bike.new }
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock Bike.new }
       expect { subject.dock Bike.new }.to raise_error "No space available, try a different docking station"
     end
   end
