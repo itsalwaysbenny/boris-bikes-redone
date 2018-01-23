fb1c63f1e52409fd49b8336af78e3d1e2792287d
Ben Taylor <taylor.ben89@gmail.com>
2018-01-23 13:23:15 +0000

    completed step 17
    All tests passing and refactored

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,19 +5,21 @@ class DockingStation
   DEFAULT_CAPACITY = 20
   attr_reader :capacity
 
-  def initialize(capacity)
+  def initialize(capacity=DEFAULT_CAPACITY)
     @bikes = []
     @capacity = capacity
   end
 
   def release_bike
     fail "No bikes available" if empty?
+    @capacity +=1
     @bikes.pop
   end
 
   def dock(bike)
     fail "No space available, try a different docking station" if full?
     @bikes << bike
+    @capacity -= 1
   end
 
   private
@@ -25,7 +27,7 @@ class DockingStation
   attr_reader :bike
 
   def full?
-    @bikes.count >= capacity
+    @capacity <= 0
   end
 
   def empty?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,56 +4,55 @@ require 'bike'
 describe DockingStation do
 
   it "responds to release_bike" do
-    expect(DockingStation.new(10)).to respond_to :release_bike
+    expect(DockingStation.new).to respond_to :release_bike
   end
 
   describe "initialization" do
-    subject { DockingStation.new(20) }
+    subject { DockingStation.new }
     let(:bike) { Bike.new }
     it "defaults capacity" do
-      described_class::DEFAULT_CAPACITY.times do
-        subject.dock(bike)
-      end
-      expect{ subject.dock(bike) }.to raise_error "No space available, try a different docking station"
+      ds = DockingStation.new
+      20.times { ds.dock(:bike) }
+      expect{ ds.dock(:bike) }.to raise_error "No space available, try a different docking station"
     end
   end
 
   describe "#release_bike" do
-    it "releseases a bike" do
-      bike = Bike.new
-      ds = DockingStation.new(20)
-      ds.dock(bike)
-      expect(ds.release_bike).to eq bike
+    subject { DockingStation.new }
+    let(:bike) { Bike.new }
+    it "releases a bike" do
+      subject.dock(:bike)
+      expect(subject.release_bike).to eq :bike
     end
   end
 
   describe "#release_bike" do
     it "raises an error when there are no bikes available" do
-      ds = DockingStation.new(0)
-      expect { ds.release_bike}.to raise_error "No bikes available"
+      subject { DockingStation.new }
+      expect { subject.release_bike}.to raise_error "No bikes available"
     end
   end
 
   it "responds to dock" do
-    expect(DockingStation.new(10)).to respond_to(:dock).with(1).argument
+    expect(DockingStation.new).to respond_to(:dock).with(1).argument
   end
 
   describe "#dock" do
     it "rasies an error when docking station is full" do
-      ds = DockingStation.new(20)
-      ds.capacity.times { ds.dock Bike.new }
-      expect { ds.dock Bike.new }.to raise_error "No space available, try a different docking station"
+      subject { DockingStation.new }
+      described_class::DEFAULT_CAPACITY.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error "No space available, try a different docking station"
     end
   end
 
   it "has a default capacity" do
-    expect(DockingStation.new(20).capacity).to eq DockingStation::DEFAULT_CAPACITY
+    expect(DockingStation.new.capacity).to eq DockingStation::DEFAULT_CAPACITY
   end
 
   describe "initialization" do
     it "has a variable capacity" do
-      docking_station = DockingStation.new(10)
-      10.times { docking_station.dock Bike.new }
+      docking_station = DockingStation.new
+      described_class::DEFAULT_CAPACITY.times { docking_station.dock Bike.new }
       expect{ docking_station.dock Bike.new }.to raise_error "No space available, try a different docking station"
     end
   end
